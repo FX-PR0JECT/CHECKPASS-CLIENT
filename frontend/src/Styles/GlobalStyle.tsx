1f12ebd0fc4519f485c99013789fa62866a92151
ccconac <qwerpoiu91@naver.com>
2024-01-02 21:11:20 +0900

    CHECKPASS-82 chore: [Client] 글로벌 스타일 설정

---
+++ frontend/src/Styles/GlobalStyle.tsx
@@ -0,0 +1,60 @@
+import { createGlobalStyle } from 'styled-components';
+import AppleTea from '../Assets/Font/AppleTea-z8R1a.woff';
+import AppleGothicL from '../Assets/Font/AppleSDGothicNeoL.woff';
+import AppleGothicR from '../Assets/Font/AppleSDGothicNeoM.woff';
+import AppleGothicB from '../Assets/Font/AppleSDGothicNeoB.woff';
+import AppleGothicEB from '../Assets/Font/AppleSDGothicNeoEB.woff';
+
+export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'AppleTea';
+    src: url(${AppleTea});
+  }
+
+  @font-face {
+    font-family: 'AppleGothicL';
+    src: url(${AppleGothicL});
+  }
+
+  @font-face {
+    font-family: 'AppleGothicR';
+    src: url(${AppleGothicR});
+  }
+
+  @font-face {
+    font-family: 'AppleGothicB';
+    src: url(${AppleGothicB});
+  }
+
+  @font-face {
+    font-family: 'AppleGothicEB';
+    src: url(${AppleGothicEB});
+  }
+
+  * {
+    padding: 0;
+    margin: 0;
+
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    min-width: 100%;
+    min-height: 100%;
+  }
+
+  body {
+    width: 100%;
+    height: 100%;
+    
+    font-family: 'NanumBarunGothic';
+  }
+
+  div {
+    display: block;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GlobalStyle } from './Styles/GlobalStyle';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <GlobalStyle />
+  </React.StrictMode>
+);
