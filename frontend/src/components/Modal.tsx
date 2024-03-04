21ae1e0b68a6439d213faf4fe0dbb03aee444338
ccconac <qwerpoiu91@naver.com>
2024-03-04 15:25:47 +0900

    CHECKPASS-269 feat: [Client] 수강 신청 구현

---
+++ frontend/src/components/Modal.tsx
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+
+const Modal = ({ text, isClose }: any) => {
+  if (!isClose) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <Background onClick={isClose}></Background>
+      <ModalContainer>
+        <Header>
+          <CloseButton onClick={isClose}>
+            <i className="fa-solid fa-xmark"></i>
+          </CloseButton>
+        </Header>
+        <Message>
+          <span>{text}</span>
+        </Message>
+      </ModalContainer>
+    </Container>
+  );
+};
+
+export default Modal;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  position: absolute;
+
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const ModalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  position: fixed;
+
+  width: 450px;
+  height: 200px;
+
+  border: 1px solid #4e4e4e;
+  border-radius: 20px;
+
+  background-color: #fff;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
+  padding: 13px;
+`;
+
+const CloseButton = styled.button`
+  border: none;
+  background-color: transparent;
+
+  font-size: 26px;
+  cursor: pointer;
+`;
+
+const Message = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: calc(200px - 100px);
+`;
