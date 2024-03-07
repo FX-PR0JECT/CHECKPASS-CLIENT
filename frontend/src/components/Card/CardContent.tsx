96c685bc642a55865f05b3f81950403afda9c739
ccconac <qwerpoiu91@naver.com>
2024-03-07 22:06:19 +0900

    CHECKPASS-224 refactor: [Client] CardContent Component 분리

---
+++ frontend/src/components/Card/CardContent.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { fontSizes } from '../../Styles/theme';
+
+type ContentPropsType = {
+  title: string;
+  hashtag: string;
+};
+
+const CardContent = ({ title, hashtag }: ContentPropsType) => {
+  return (
+    <Content>
+      <CardTitle>{title}</CardTitle>
+      <HashTag>{hashtag}</HashTag>
+    </Content>
+  );
+};
+
+export default CardContent;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const CardTitle = styled.span`
+  font-family: 'AppleGothicB';
+  font-size: 1.6rem;
+`;
+
+const HashTag = styled.span`
+  word-spacing: 8px;
+  font-size: ${fontSizes.medium};
+  color: #6d6d6d;
+`;
