0d09a273546485e3fb49263b928f16835eb2da86
ccconac <qwerpoiu91@naver.com>
2024-03-10 17:16:01 +0900

    CHECKPASS-293 feat: [Client] 메인 페이지 카드 Link 추가

--- frontend/src/constants/cardData.ts
+++ frontend/src/constants/cardData.ts
@@ -7,13 +7,15 @@ const CARD_DATA = Object.freeze([
     content: '수강 신청',
     title: '수강 신청 바로 가기',
     hashtag: '#개설강의목록 #수강신청내역',
+    link: '/enrollment',
   },
   {
     image: IMAGE.CalendarIcon,
     description: 'CalendarIcon',
-    content: '비콘이 이상하다면?',
+    content: '학기 시간표가 궁금하다면?',
     title: '시간표 확인하기',
     hashtag: '#학기시간표 #강의확인',
+    link: '/lecture',
   },
   {
     image: IMAGE.CommunityIcon,
@@ -21,6 +23,7 @@ const CARD_DATA = Object.freeze([
     content: '수강생 의견',
     title: '자유롭게 소통하기',
     hashtag: '#의견 #소통 #정보공유',
+    link: '/',
   },
   {
     image: IMAGE.NoticeIcon,
@@ -28,6 +31,7 @@ const CARD_DATA = Object.freeze([
     content: '과제가 궁금하다면?',
     title: '공지 확인하기',
     hashtag: '#일정 #과제 #공지',
+    link: '/',
   },
 ]);
 
