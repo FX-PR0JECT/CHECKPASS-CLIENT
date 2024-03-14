bb6f80f2fa2828eefbdb655a84a35e7cda09e9d5
ccconac <qwerpoiu91@naver.com>
2024-03-15 03:26:30 +0900

    CHECKPASS-272 feat: [Client] Form Item Component 생성

---
+++ frontend/src/Pages/Management/FormItems/FormItem.tsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import React from 'react';
+
+type ChildrenProps = {
+  children: React.ReactNode;
+};
+
+const FormItem = ({ children }: ChildrenProps) => {
+  return <Item>{children}</Item>;
+};
+
+const Label = ({ children }: ChildrenProps) => {
+  return <span>{children}</span>;
+};
+
+type InputProps = {
+  placeholder: string;
+  type: 'text';
+};
+
+const Input = ({ placeholder, type }: InputProps) => {
+  return <InputTag type={type} placeholder={placeholder} />;
+};
+
+type Option = {
+  value: string;
+  name: string;
+};
+
+type SelectProps = {
+  options: Option[];
+};
+
+const Select = ({ options }: SelectProps) => {
+  return (
+    <SelectTag>
+      {options.map((option, index) => (
+        <option key={index} value={option.value}>
+          {option.name}
+        </option>
+      ))}
+    </SelectTag>
+  );
+};
+
+export { FormItem, Label, Input, Select };
+
+const Item = styled.form`
+  display: flex;
+  align-items: center;
+
+  gap: 5px;
+
+  font-size: 15px;
+`;
+
+const InputTag = styled.input`
+  padding: 3px 8px;
+
+  border: 1px solid #5a5a5a;
+  border-radius: 5px;
+`;
+
+const SelectTag = styled.select`
+  padding: 3px 3px;
+
+  border: 1px solid #5a5a5a;
+  border-radius: 5px;
+`;
