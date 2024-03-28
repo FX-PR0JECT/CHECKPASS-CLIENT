89a09553b4ed6d304e09af37969732fba721ee36
areumH <94692566+areumH@users.noreply.github.com>
2024-03-28 13:04:34 +0900

    CHECKPASS-341 feat: [Client] 특정 강의 실시간 출석 현황 표시 구현

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { GlobalStyle } from './Styles/GlobalStyle';
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import router from './router';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:3000/';
 axios.defaults.withCredentials = true;
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <GlobalStyle />
-    <RouterProvider router={router} />
-  </React.StrictMode>
+ReactDOM.render(
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </QueryClientProvider>,
+  document.getElementById('root')
 );
